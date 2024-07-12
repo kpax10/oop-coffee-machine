4d254884c30c0daa8e35fd01c6ab87412e423c75
Kyle <notxap55@yahoo.com>
2024-07-11 21:34:04 -0700

    completed

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
+
+coffee = True
+
+while coffee:
+    order = input("What do you want to order? latte/espresso/cappuccino ").lower()
+    if order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif order == 'off':
+        coffee = False
+    else:
+        drink = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
